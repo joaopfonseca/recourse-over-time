1a96b060c140519be3f36c45f211dfcb6397f233
joaopfonseca <jpmrfonseca@gmail.com>
2023-02-16 23:59:13 +0000

    Update mock experiment

--- scripts/mock_experiment.py
+++ scripts/mock_experiment.py
@@ -2,33 +2,25 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-from recourse import ActionSet
-
-from algorec import BaseEnvironment, BasePopulation, ActionableRecourse
+from algorec import BasePopulation, ActionableRecourse
 
 rng = np.random.default_rng(42)
-df = pd.DataFrame(rng.random((100, 3)), columns=["a", "b", "c"])
+df = pd.DataFrame(rng.random((100, 4)), columns=["a", "b", "c", "d"])
 y = rng.integers(0, 2, 100)
 
-lr = LogisticRegression().fit(df.values, y)
+lr = LogisticRegression().fit(df, y)
 y_pred = lr.predict(df)
 
 # Testing a single counterfactual
-population = BasePopulation(data=df, y_desired=1)
+population = BasePopulation(
+    data=df,
+    y_desired=1,
+    categorical=None,
+    immutable=["b"],
+    step_direction={"a": -1}
+)
 
-# Explicitly defining an action set should not be necessary in the future
-act_set = ActionSet(X=df, y_desired=1)
-population.action_set(act_set)
+# TODO: test for y_desired = 0
 
 recourse_algo = ActionableRecourse(lr, threshold=0.65, flipset_size=100)
 cfs = recourse_algo.counterfactuals(population)
-
-
-# Test a population within an environment - not working yet
-population = BasePopulation(data=rng.random((100, 3)))
-env = BaseEnvironment(
-    population=population,
-    decision_model=lr,
-    threshold_definition=20,  # A fixed number of observations
-    recourse_method=ActionableRecourse(),
-)
