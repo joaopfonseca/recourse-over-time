86bdde4be6defe375650892ad445dc69edf07f00
joaopfonseca <jpmrfonseca@gmail.com>
2023-02-23 15:26:19 +0000

    MNT move ActionableRecourse to separate directory

--- algorec/recourse/__init__.py
+++ algorec/recourse/__init__.py
@@ -1,4 +1,4 @@
-from .actionable_recourse import ActionableRecourse
-from .action_set import ActionSet
+from .actionable_recourse.actionable_recourse import ActionableRecourse
+from .actionable_recourse.action_set import ActionSet
 
 __all__ = ["ActionableRecourse", "ActionSet"]
