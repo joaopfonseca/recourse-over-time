41a75037333b3669799a538b8040c06a821cd5e6
joaopfonseca <jpmrfonseca@gmail.com>
2023-02-27 16:52:29 +0000

    FIX Bug fixes + visual improvements

--- algorec/visualization/plot_environment.py
+++ algorec/visualization/plot_environment.py
@@ -19,32 +19,6 @@ class EnvironmentPlot:
         self.environment = environment
         self.random_state = random_state
 
-    def _create_mesh_grid(self, X, mesh_size):
-        self.mesh_x, self.mesh_y = [
-            np.array(
-                np.linspace(
-                    self.min_X[i] - 0.2 * (self.max_X[i] - self.min_X[i]),
-                    self.max_X[i] + 0.2 * (self.max_X[i] - self.min_X[i]),
-                    mesh_size[i],
-                )
-            )
-            for i in range(2)
-        ]
-        mesh = np.stack(np.meshgrid(self.mesh_x, self.mesh_y), axis=2)
-        mesh = np.reshape(mesh, (mesh_size[0] * mesh_size[1], 2))
-
-        if self.is_large_dim:
-            mesh = (
-                np.dot(mesh, self.autoencoder.coefs_[1])
-                + self.autoencoder.intercepts_[1]
-            )
-
-        mesh = self.environment.model_.predict_proba(
-            pd.DataFrame(mesh, columns=X.columns)
-        )[:, -1]
-        self.mesh_ = np.reshape(mesh, mesh_size)
-        return self
-
     def fit(self, X=None):
         if X is None:
             X = self.environment.population.data
@@ -69,12 +43,53 @@ class EnvironmentPlot:
                 columns=["Component 1", "Component 2"],
             )
 
-        self.min_X = X.min(0)
-        self.max_X = X.max(0)
+        self._min_x = X.min(0)
+        self._max_x = X.max(0)
 
         return self
 
-    def scatter(self, step=None, mesh_size=(100, 100)):
+    def _create_mesh_grid(self, X_curr, mesh_size):
+
+        X_ = pd.DataFrame(
+            (
+                np.dot(X_curr.values, self.autoencoder.coefs_[0])
+                + self.autoencoder.intercepts_[0]
+            ),
+            index=X_curr.index,
+            columns=["Component 1", "Component 2"],
+        )
+        _min_x_curr = X_.min(0)
+        _max_x_curr = X_.max(0)
+
+        mins = np.array([_min_x_curr, self._min_x]).min(0)
+        maxs = np.array([_max_x_curr, self._max_x]).max(0)
+
+        self.mesh_x, self.mesh_y = [
+            np.array(
+                np.linspace(
+                    mins[i] - 0.1 * (maxs[i] - mins[i]),
+                    maxs[i] + 0.1 * (maxs[i] - mins[i]),
+                    mesh_size[i],
+                )
+            )
+            for i in range(2)
+        ]
+        mesh = np.stack(np.meshgrid(self.mesh_x, self.mesh_y), axis=2)
+        mesh = np.reshape(mesh, (mesh_size[0] * mesh_size[1], 2))
+
+        if self.is_large_dim:
+            mesh = (
+                np.dot(mesh, self.autoencoder.coefs_[1])
+                + self.autoencoder.intercepts_[1]
+            )
+
+        mesh = self.environment.model_.predict_proba(
+            pd.DataFrame(mesh, columns=X_curr.columns)
+        )[:, -1]
+        self.mesh_ = np.reshape(mesh, mesh_size)
+        return self
+
+    def scatter(self, step=None, mesh_size=(100, 100), legend=True, title=True):
         """Visualize the population in a 2d-scatter plot."""
         if not hasattr(self, "autoencoder"):
             self.fit()
@@ -82,13 +97,13 @@ class EnvironmentPlot:
         if step is None:
             step = self.environment.step_
 
-        self._create_mesh_grid(self.environment.population.data, mesh_size)
-
         df = self.environment.metadata_[step]["population"].data
         if step > 0:
             df_prev = self.environment.metadata_[step - 1]["population"].data
         threshold = self.environment.metadata_[step]["threshold"]
         mask = self.environment.model_.predict_proba(df)[:, -1] >= threshold
+
+        self._create_mesh_grid(df, mesh_size)
 
         if self.is_large_dim:
             df = pd.DataFrame(
@@ -144,25 +159,30 @@ class EnvironmentPlot:
                 y=df_prev.loc[idx].iloc[:, 1],
                 alpha=0.3,
                 color=self._previous,
-                label="Prev. position",
+                label="Prev. position" if legend else None,
             )
 
         ax.scatter(
             x=df.iloc[~mask, 0],
             y=df.iloc[~mask, 1],
             color=self._unfavorable,
-            label="Unfavorable",
+            label="Unfavorable" if legend else None,
         )
         ax.scatter(
             x=df.iloc[mask, 0],
             y=df.iloc[mask, 1],
             color=self._favorable,
-            label="Favorable",
+            label="Favorable" if legend else None,
         )
-        ax.legend()
+        if legend:
+            ax.legend()
+        if title:
+            ax.set_title(f"Population at t={step}")
+        ax.set_xlabel(df.columns[0])
+        ax.set_ylabel(df.columns[1])
         return fig, ax
 
-    def agent_scores(self, min_step=None, max_step=None):
+    def agent_scores(self, min_step=None, max_step=None, legend=True, title=True):
         """Visualize population scores across multiple time steps."""
         if not hasattr(self, "autoencoder"):
             self.fit()
@@ -184,19 +204,21 @@ class EnvironmentPlot:
             x = np.ones(df.shape[0], dtype=int) * step
             x = x + swarm(prob)
 
+            add_labels = legend and step in [min_step, 0]
+
             ax.scatter(
                 x=x[~mask],
                 y=prob[~mask],
                 color=self._unfavorable,
                 alpha=0.5,
-                label=("Unfavorable" if step in [min_step, 0] else None),
+                label=("Unfavorable" if add_labels else None),
             )
             ax.scatter(
                 x=x[mask],
                 y=prob[mask],
                 color=self._favorable,
                 alpha=0.5,
-                label=("Favorable" if step in [min_step, 0] else None),
+                label=("Favorable" if add_labels else None),
             )
 
         ax.plot(
@@ -207,9 +229,12 @@ class EnvironmentPlot:
             alpha=0.3,
         )
 
+        if title:
+            ax.set_title(f"Population scores over {len(df_list)} time steps")
+        if legend:
+            ax.legend()
         ax.set_xticks([meta[0] for meta in df_list])
         ax.set_xlabel("Step")
         ax.set_ylabel("Score")
 
-        ax.legend()
         return fig, ax
